3d9390e3f1bbee066d747dbddf3efac97f1c520d
ahmet emrebas <noreply@authdare.com>
2022-11-28 19:42:00 -0600

    clean up

--- apps/inventory-desktop/src/app/constants.ts
+++
@@ -1,4 +0,0 @@
-export const rendererAppPort = 4200;
-export const rendererAppName = 'inventory';
-export const electronAppName = 'inventory-desktop';
-export const updateServerUrl = 'https://deployment-server-url.com'; // TODO: insert your update server url here
