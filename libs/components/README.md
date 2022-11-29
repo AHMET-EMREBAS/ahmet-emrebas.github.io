ffd085bc3f56b1dad2917bfcb9dac2261ec7019c
ahmet emrebas <noreply@authdare.com>
2022-11-28 20:10:59 -0600

    icons

--- libs/components/README.md
+++ libs/components/README.md
@@ -1,7 +1,7 @@
-# material
+# components
 
 This library was generated with [Nx](https://nx.dev).
 
 ## Running unit tests
 
-Run `nx test material` to execute the unit tests.
+Run `nx test components` to execute the unit tests.
--- libs/components/jest.config.ts
+++ libs/components/jest.config.ts
@@ -1,6 +1,6 @@
 /* eslint-disable */
 export default {
-  displayName: 'material',
+  displayName: 'components',
   preset: '../../jest.preset.js',
   setupFilesAfterEnv: ['<rootDir>/src/test-setup.ts'],
   globals: {
@@ -9,7 +9,7 @@ export default {
       stringifyContentPathRegex: '\\.(html|svg)$',
     },
   },
-  coverageDirectory: '../../coverage/libs/material',
+  coverageDirectory: '../../coverage/libs/components',
   transform: {
     '^.+\\.(ts|mjs|js|html)$': 'jest-preset-angular',
   },
