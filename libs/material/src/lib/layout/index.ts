acc5f286a88b06eda2206165b3c8a682fe2efc35
Ahmet Emrebas <aemrebas.dev@gmail.com>
2023-09-16 12:31:47 -0500

    build: webapp built successfully

--- libs/material/src/lib/layout/index.ts
+++ libs/material/src/lib/layout/index.ts
@@ -2,3 +2,4 @@ export * from './layout.module';
 export * from './directives'
 export * from './mobile-navigation'
 export * from './navigation'
+export * from './layout.component';
--- libs/material/src/lib/layout/layout.component.ts
+++ libs/material/src/lib/layout/layout.component.ts
@@ -1,11 +1,4 @@
-import {
-  ChangeDetectionStrategy,
-  ChangeDetectorRef,
-  Component,
-  ElementRef,
-  ViewChild,
-  inject,
-} from '@angular/core';
+import { ChangeDetectionStrategy, Component, inject } from '@angular/core';
 import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
 import { Observable } from 'rxjs';
 import { map, shareReplay } from 'rxjs/operators';
