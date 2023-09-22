4ef466bcff365063879ddd0528f51a2448a26d22
Ahmet Emrebas <aemrebas.dev@gmail.com>
2023-09-21 22:32:24 -0500

    material: button styles

--- libs/material/src/colors/colors.component.ts
+++ libs/material/src/colors/colors.component.ts
@@ -21,6 +21,21 @@ export class ColorsComponent {
   ];
   colorVariants = this.colors.map((e) => [
     e,
-    ...['light', 'lighter', 'dark', 'darker'].map((v) => e + '-' + v),
+    ...[
+      'light',
+      'lighter',
+      'dark',
+      'darker',
+      1,
+      2,
+      3,
+      4,
+      5,
+      6,
+      7,
+      8,
+      9,
+      10,
+    ].map((v) => e + '-' + v),
   ]);
 }
