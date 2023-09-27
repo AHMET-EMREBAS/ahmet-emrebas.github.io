e272452f0b2b9af55a2eda969ca4c5a97ef331fa
Ahmet Emrebas <aemrebas.dev@gmail.com>
2023-09-27 06:38:59 -0500

    material: multi select

--- libs/material/src/form/multiple-select-input/multiple-select-input.component.stories.ts
+++ libs/material/src/form/multiple-select-input/multiple-select-input.component.stories.ts
@@ -1,12 +1,12 @@
 import type { Meta, StoryObj } from '@storybook/angular';
 import { MultipleSelectInputComponent } from './multiple-select-input.component';
 
-import { within } from '@storybook/testing-library';
+import { userEvent, within } from '@storybook/testing-library';
 import { expect } from '@storybook/jest';
 
 const meta: Meta<MultipleSelectInputComponent> = {
   component: MultipleSelectInputComponent,
-  title: 'MultipleSelectInputComponent',
+  title: 'Input Multi Select',
 };
 export default meta;
 type Story = StoryObj<MultipleSelectInputComponent>;
@@ -16,9 +16,30 @@ export const Primary: Story = {
 };
 
 export const Heading: Story = {
-  args: {},
+  args: {
+    value: '',
+    name: 'category',
+    autocomplete: 'off',
+    color: 'primary',
+    label: 'category',
+    variant: 'basic',
+    icon: 'category',
+    options: [
+      { icon: 'computer', value: 'PC' },
+      { icon: 'bed', value: 'Furniture' },
+      { icon: 'water', value: 'Food' },
+    ],
+  },
   play: async ({ canvasElement }) => {
     const canvas = within(canvasElement);
-    expect(canvas.getByText(/multiple-select-input works!/gi)).toBeTruthy();
+    expect(canvas.getByLabelText(/Category/gi)).toBeTruthy();
+    const elem = canvas.getByLabelText(/Category/gi);
+    userEvent.click(elem);
+
+    const option1 = canvas.getByTestId('B 1');
+
+    userEvent.click(option1, { delay: 200 });
+
+    // userEvent.selectOptions(elem, 'Option 1');
   },
 };
--- libs/material/src/form/multiple-select-input/multiple-select-input.component.ts
+++ libs/material/src/form/multiple-select-input/multiple-select-input.component.ts
@@ -21,8 +21,12 @@ import { SelectOption } from '../../api';
           [name]="name"
           [autocomplete]="autocomplete"
           (input)="getOptions()"
+          (keydown)="handleKeyDown($event)"
           tbHasValue
         />
+      </div>
+      <div class="selected-items">
+        {{ selectedItems() }}
       </div>
       <div class="dropdown-items {{ distribution }} w-full h-10em oy-auto ">
         <div
@@ -43,18 +47,63 @@ import { SelectOption } from '../../api';
     </div>
   `,
 })
-export class MultipleSelectInputComponent extends CommonInputComponent {
+export class MultipleSelectInputComponent extends CommonInputComponent<string> {
+  selectedItems() {
+    return (
+      this.options
+        ?.filter((e) => e.selected)
+        .map((e) => e.value)
+        .join(',') || ''
+    );
+  }
   selectOption(option: SelectOption) {
     option.selected = !option.selected;
+    this.inputRef.nativeElement.value = this.selectedItems() + ',';
+    this.inputRef.nativeElement.dispatchEvent(
+      new InputEvent('input', {
+        bubbles: true,
+        cancelable: true,
+        composed: true,
+      })
+    );
+    this.inputRef.nativeElement.focus();
   }
 
   override emit(): void {
-    this.inputEvent.emit(this.options?.filter((e) => e.selected));
+    this.inputEvent.emit(this.selectedItems());
+  }
+  handleInput() {
+    const selectedItems = this.value!.split(',');
+    const lastSelected = selectedItems!.pop()!;
+  }
+  searchText() {
+    return this.value?.split(',').pop()?.toLowerCase() || '';
   }
 
+  handleKeyDown(event: KeyboardEvent) {
+    console.log(event);
+    if (event.key === 'Tab') {
+      event.preventDefault();
+      const option = this.getOptions()?.shift();
+      if (option) {
+        this.selectOption(option);
+      }
+    }
+
+    if (event.key === 'Backspace') {
+      event.preventDefault();
+      const list = this.inputRef.nativeElement.value.split(',');
+      const deletedItem = list.pop();
+      const item = this.options?.find((e) => e.value === deletedItem);
+      if (item) {
+        item.selected = false;
+      }
+      this.inputRef.nativeElement.value = list.join(',');
+    }
+  }
   getOptions() {
-    return this.options?.filter((e) =>
-      e.value.toLowerCase().includes(this.value.toLowerCase())
-    );
+    return this.options?.filter((e) => {
+      return e.value.toLowerCase().includes(this.searchText()) && !e.selected
+    });
   }
 }
