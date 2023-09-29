a3e9d1d0843b6ef164f6bb361c7e3af3c70602fb
Ahmet Emrebas <aemrebas.dev@gmail.com>
2023-09-29 03:35:40 -0500

    material: multi select inputs

--- libs/material/src/form/multiple-select-input/multiple-select-input.component.ts
+++ libs/material/src/form/multiple-select-input/multiple-select-input.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component } from '@angular/core';
 import { CommonInputComponent } from '../common-input/common-input.component';
 import { MicroModule } from '../../micro/micro.module';
 import { SelectOption } from '../../api';
@@ -10,57 +10,100 @@ import { SelectOption } from '../../api';
   template: `
     <div class="dropdown input-dropdown {{ direction }}  {{ color }}">
       <div class="input-container {{ color }} {{ variant }}">
-        <span class="icon"> {{ icon }}</span>
-        <label [for]="id()">{{ label }} </label>
+        <label [for]="id()"></label>
+        <span class="icon">{{ icon }}</span>
         <input
           #input
-          [(ngModel)]="searchtext"
-          type="text"
+          [(ngModel)]="value"
           [id]="id()"
-          [attr.data-testid]="name"
-          [name]="name"
-          [autocomplete]="autocomplete"
-          (input)="filterOptions()"
-          tbHasValue
+          type="text"
+          (input)="inputHandler()"
+          (keydown)="keydownHandler($event)"
         />
-
-        <!--TODO :  Add closeable chip here -->
-        <span>{{ selectedItems() }}</span>
       </div>
 
-      <div class="dropdown-items {{ distribution }} w-full h-10em oy-auto ">
+      <div class="dropdown-items {{ distribution }}  w-full h-10em oy-auto ">
         <div
-          [attr.data-testid]="option.value"
-          [id]="id() + option.value"
-          class="nav-list-item inline-input cursor-pointer {{ color }}"
-          *ngFor="let option of filterOptions()"
+          class="list-item {{ color }}"
+          *ngFor="let option of filteredOptions"
           (click)="selectOption(option)"
         >
-          <tb-icon [icon]="option.icon"></tb-icon>
-          <span>{{ option.value }}</span>
-          <div class="grow-1"></div>
-          <span class="icon {{ color }}-face" *ngIf="option.selected">
-            check
-          </span>
+          <span
+            class="icon"
+            *ngIf="option.icon"
+            [class.accent]="option.selected"
+            >{{ option.icon }}</span
+          >
+          <span> {{ option.label || option.value }}</span>
         </div>
       </div>
     </div>
   `,
 })
-export class MultipleSelectInputComponent extends CommonInputComponent<string> {
-  searchtext = '';
+export class MultipleSelectInputComponent
+  extends CommonInputComponent<string>
+  implements AfterViewInit
+{
+  filteredOptions: SelectOption[] = [];
+
+  ngAfterViewInit(): void {
+    this.filteredOptions = [...(this.options || [])];
+  }
+
+  inputHandler() {
+    const lastText = this.value?.split(',').pop()?.toLowerCase() || '';
+
+    if (lastText?.length > 0) {
+      this.filteredOptions = [
+        ...(this.options?.filter((e) => {
+          return e.value.toLowerCase().includes(lastText.toLowerCase());
+        }) || []),
+      ];
+    } else {
+      this.filteredOptions = this.options || [];
+    }
+    this.emit();
+  }
+
+  backspaceHandler(event: KeyboardEvent) {
+    event.preventDefault();
+    const values = this.value?.split(',');
+    values?.splice(-1, 1);
+    this.value = values?.join(',');
+
+    this.options?.forEach((e) => {
+      if (this.value?.includes(e.value)) {
+        e.selected = true;
+      } else {
+        e.selected = false;
+      }
+    });
+  }
+
+  enterHandler(event: KeyboardEvent) {
+    const option = this.filteredOptions[0];
+    this.selectOption(option);
+  }
+
+  keydownHandler(event: KeyboardEvent) {
+    if (event.key === 'Enter') {
+      this.enterHandler(event);
+    } else if (event.key === 'Backspace') {
+      this.backspaceHandler(event);
+    }
+  }
 
   selectOption(option: SelectOption) {
-    option.selected = !option.selected;
-  }
-
-  filterOptions(): SelectOption[] | undefined {
-    return this.options?.filter((e) =>
-      e.value?.toLowerCase().includes(this.searchtext?.toLowerCase() || '')
-    );
-  }
-
-  selectedItems() {
-    return this.options?.filter((e) => e.selected);
+    const found = this.options?.find((e) => e.value === option.value);
+    if (found) {
+      found.selected = !found.selected;
+    }
+    this.value =
+      this.options
+        ?.filter((e) => e.selected)
+        .map((e) => e.value)
+        .join(',') + ',';
+    this.filteredOptions = [...(this.options || [])];
+    this.emit();
   }
 }
